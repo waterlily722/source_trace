10f7daf4b661b2028f57f1c0bacd8cfb015ace21
waterlily722 <92020563+waterlily722@users.noreply.github.com>
2023-03-19 15:07:24 +0800

    Update sequence.py

--- sequence.py
+++ sequence.py
@@ -1,4 +1,17 @@
-def sort_repos(repo_results):
-    sorted_repos = sorted(repo_results, key=lambda repo: (-repo["forks_count"],
-                                                          -repo["stargazers_count"], repo["created_at"]))
-    return sorted_repos
+from typing import List
+from library import get_repo_properties
+
+
+def sort_repositories(repositories: List[str]) -> List[str]:
+    properties = []
+    for repo in repositories:
+        repository_property = get_repo_properties(repo)
+        properties.append((repo, repository_property))
+
+    # 排序
+    sorted_repositories = sorted(
+        properties,
+        key=lambda x: (-x[1]['forks_count'], -x[1]['stargazers_count'], x[1]['created_at'])
+    )
+
+    return [repo[0] for repo in sorted_repositories]
