5b061c341275035ccfb6c7a6be2f4b5195291d12
waterlily722 <92020563+waterlily722@users.noreply.github.com>
2023-03-19 15:04:39 +0800

    Update execute.py

--- execute.py
+++ execute.py
@@ -1,9 +1,10 @@
 import requests
 
+
 def execute_query(query):
     url = "http://api.github.com/search/code"
     headers = {'User-Agent': 'Mozilla/5.0',
-               'Authorization': 'token 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',
+               'Authorization': 'token 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',
                'Content-Type': 'application/json',
                'Accept': 'application/json'
 
@@ -13,13 +14,42 @@ def execute_query(query):
     items = results.get("items", [])
 
     if response.status_code != 200:
+        print(response.status_code)
         print('error: fail to request')
 
     # 提取需要的信息并存储在列表中
     search_results = []
+    repository = []
     for item in items:
         repo_name = item["repository"]["full_name"]
         result = {"name": item["name"], "path": item["path"], "repo_name": repo_name}
+        repository.append(repo_name)
         search_results.append(result)
 
     return search_results
+
+
+def get_repository(query):
+    url = "http://api.github.com/search/code"
+    headers = {'User-Agent': 'Mozilla/5.0',
+               'Authorization': 'token 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',
+               'Content-Type': 'application/json',
+               'Accept': 'application/json'
+
+               }
+    response = requests.get(url, headers=headers, params=query)
+    results = response.json()
+    items = results.get("items", [])
+
+    if response.status_code != 200:
+        print(response.status_code)
+        print('error: fail to request')
+
+    # 提取需要的信息并存储在列表中
+    search_results = []
+    repository = []
+    for item in items:
+        repo_name = item["repository"]["full_name"]
+        repository.append(repo_name)
+
+    return repository
